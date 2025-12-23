4254cab95f608df7908a5732c10b04b19a473e2a
butkopa <info@betogether.lv>
2025-12-23 16:24:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const popup = document.getElementById("popup");
 const popupTitle = document.getElementById("popupTitle");
 const popupText = document.getElementById("popupText");
+const popupImg = document.getElementById("popupImg");
 
 function openPopup(person) {
   if (person === "anna") {
     popupTitle.innerText = "Anna, 7";
-    popupText.innerText = "Anna needs urgent medical support.";
+    popupText.innerText = "Annai nepieciešama medicīniskā palīdzība.";
+    popupImg.src = "images/anna.jpg";
   }
 
   if (person === "mark") {
-    popupTitle.innerText = "Mark, 12";
-    popupText.innerText = "Mark needs educational support.";
+    popupTitle.innerText = "Marks, 12";
+    popupText.innerText = "Markam nepieciešams izglītības atbalsts.";
+    popupImg.src = "images/mark.jpg";
   }
 
   popup.style.display = "flex";
